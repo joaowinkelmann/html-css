2254520b632127ff9d9d003f1e7fb449b75350b0
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-21 08:41:35 -0300

    desafios final?

--- imply/semana_1/Ex01/script.js
+++ imply/semana_1/Ex01/script.js
@@ -14,7 +14,7 @@ function getEmail(){
     let email = document.querySelector('#email').value;
     const regemail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(email != "" && regemail.test(email)){
-        document.querySelector('#emailusuario').innerHTML = `O e-mail informado pelo usuário foi ${email}`;
+        document.querySelector('#emailusuario').innerHTML += `<br>O e-mail informado pelo usuário foi ${email}`;
     }
     else{
         window.alert("Erro: E-mail inválido!");
@@ -36,4 +36,10 @@ function darkMode(){
     document.querySelector('body').style.background = 'rgb(80, 80, 80)';
     document.querySelector('body').style.color = 'rgb(230, 230, 230)';
     document.querySelector('.container').style.background = 'rgb(90,90,90)';
+}
+
+function lightMode(){
+    document.querySelector('body').style.background = 'rgb(255, 255, 255)';
+    document.querySelector('body').style.color = 'rgb(0, 0, 0)';
+    document.querySelector('.container').style.background = 'rgb(240,240,240)';
 }
